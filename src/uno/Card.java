e8ce2b3f1f48adf3adaf61abb940c21327de6818
Ford Polia <poliaf@wit.edu>
2016-03-03 08:36:22 -0500

    Finished card class

--- src/uno/Card.java
+++ src/uno/Card.java
@@ -1,7 +1,11 @@
 package uno;
 
 public class Card implements Comparable<Card>{
-	
+	/**
+	 * Enum for Color of an UNO card with values for comparison
+	 * @author poliaf
+	 *
+	 */
 	public static enum Color {
 		RED(0), YELLOW(1), GREEN(2), BLUE(3), WILD(4);
 		
@@ -15,7 +19,11 @@ public class Card implements Comparable<Card>{
 			return comparisonValue;
 		}
 	};
-	
+	/**
+	 * Enum for the symbol on an UNO card with values for comparison
+	 * @author poliaf
+	 *
+	 */
 	public static enum Symbol {
 		ZERO(0), ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8),
 		NINE(9), SKIP(10), REVERSE(11), DRAW_TWO(12), WILD(13), WILD_DRAW_FOUR(14);
@@ -33,20 +41,84 @@ public class Card implements Comparable<Card>{
 	
 	private Color cardColor;
 	private Symbol cardSymbol;
+	private Card previous;
+	private Card next;
 	
+	/**
+	 * 
+	 * @param Color of the card
+	 * @param Symbol on the card
+	 */
 	public Card (Color c, Symbol s){
-		cardColor = c;
+		if (s == Symbol.WILD || s == Symbol.WILD_DRAW_FOUR){
+			cardColor = Color.WILD;
+		} else {
+			cardColor = c;
+		}
 		cardSymbol = s;
 	}
-	
+	/**
+	 * @return Color of the card
+	 */
 	public Color getColor(){
 		return cardColor;
 	}
-	
+	/**
+	 * @return Symbol of the card
+	 */
 	public Symbol getSymbol(){
 		return cardSymbol;
 	}
+	/**
+	 * @param previousCard
+	 */
+	public void setPrevious(Card previousCard){
+		previous = previousCard;
+	}
+	/**
+	 * @param nextCard
+	 */
+	public void setNext(Card nextCard){
+		next = nextCard;
+	}
+	/**
+	 * @return previous card
+	 */
+	public Card getPrevious(){
+		return previous;
+	}
+	/**
+	 * @return next card
+	 */
+	public Card getNext(){
+		return next;
+	}
+	/**
+	 * @param otherCard
+	 * @return true if cards have the same color, false otherwise
+	 */
+	public boolean isColorSame(Card otherCard){
+		if (this.cardColor == otherCard.getColor()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	/**
+	 * @param otherCard
+	 * @return true if cards have the same symbol, false otherwise
+	 */
+	public boolean isSymbolSame(Card otherCard){
+		if (this.cardSymbol == otherCard.getSymbol()){
+			return true;
+		}else{
+			return false;
+		}
+	}
 	
+	/**
+	 * Returns a string stating the color and symbol on the card
+	 */
 	public String toString(){
 		String output;
 		switch (cardColor){
@@ -63,6 +135,7 @@ public class Card implements Comparable<Card>{
 			output = "Blue";
 			break;
 		case WILD:
+			//will be overwritten later
 			output = "Wild Card";
 			break;
 		default:
@@ -120,7 +193,14 @@ public class Card implements Comparable<Card>{
 		}
 		return output;
 	}
-	
+	/**
+	 * Compares the card to another card
+	 * returns 0 if the cards are the same
+	 * returns -1 if the first card has a lower symbol than the second
+	 * returns 1 otherwise
+	 * If the symbols are the same, but the colors are not, either -1 or 1 is returned
+	 * RED, YELLOW, GREEN, BLUE, WILD
+	 */
 	@Override
 	public int compareTo(Card otherCard){
 		if (this.cardColor == otherCard.getColor() &&
