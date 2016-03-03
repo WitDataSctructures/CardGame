d49acc71afb98c32419c9b292e82664bf45ffd83
Ford Polia <poliaf@wit.edu>
2016-03-03 17:01:50 -0500

    Added constant to control number of time shuffle method shuffles

--- src/uno/Deck.java
+++ src/uno/Deck.java
@@ -6,6 +6,8 @@ import adt.Pile;
 import java.util.Random;
 
 public class Deck extends Pile {
+	
+	final static int NUM_OF_SHUFFLES = 2;
 
 	/**
 	 * removes cards from the top of the deck and places them in
@@ -14,7 +16,7 @@ public class Deck extends Pile {
 	public void shuffle() {
 		Random random = new Random();
 		Card currentCard, placeAfter, placeBefore;
-		for (int i = this.getSize(); i > 0; i--){
+		for (int i = this.getSize() * 2; i > 0; i--){
 			currentCard = this.drawFromTop();
 			placeAfter = this.peekFromTop();
 			for (int j = random.nextInt(this.getSize()); j > 0; j--){
