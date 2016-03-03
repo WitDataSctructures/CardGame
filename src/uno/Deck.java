9752e4ba67fe7ebd154dbd673c2a2de4736f86a9
Ford Polia <poliaf@wit.edu>
2016-03-03 16:54:27 -0500

    Added shuffle method to deck

--- src/uno/Deck.java
+++ src/uno/Deck.java
@@ -1,10 +1,30 @@
 package uno;
 
+import adt.Card;
 import adt.Pile;
+
+import java.util.Random;
 
 public class Deck extends Pile {
 
+	/**
+	 * removes cards from the top of the deck and places them in
+	 * a random spot in the deck.
+	 */
 	public void shuffle() {
-
+		Random random = new Random();
+		Card currentCard, placeAfter, placeBefore;
+		for (int i = this.getSize(); i > 0; i--){
+			currentCard = this.drawFromTop();
+			placeAfter = this.peekFromTop();
+			for (int j = random.nextInt(this.getSize()); j > 0; j--){
+				placeAfter = placeAfter.getNext();
+			}
+			placeBefore = placeAfter.getNext();
+			placeAfter.setNext(currentCard);
+			currentCard.setNext(placeBefore);
+		}
+		
+		
 	}
 }
