530346da17726447b3fef861197939f0b9870165
Jake Mathews <mathewsj2@wit.edu>
2016-03-14 22:56:17 -0400

    Revert "Finished game setup"
    This reverts commit f26c2142f19b0f0425ef881e63dabd5397f90efa.

--- src/uno/Game.java
+++ src/uno/Game.java
@@ -1,37 +1,29 @@
 package uno;
 
-import java.util.ArrayList;
-import java.util.Random;
 import java.util.Scanner;
 
-import adt.Card;
-import adt.Card.Color;
-import adt.Card.Symbol;
+import adt.Pile;
 
 public class Game {
-	
+
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.setup();
 		game.start();
 	}
-	
+
 	private final static int MIN_PLAYERS = 2, MAX_PLAYERS = 10;
-	
+
 	private int playerCount = 0;
-	private ArrayList<Player> players;
+	private Player[] players;
 	private Scanner console;
-	private Deck discardPile;
-	private Deck pickupPile;
-	
-	// Game time variables
-	Player currentPlayer;
-	boolean reversed = false;
-	
+	private Pile discardPile;
+	private Pile pickupPile;
+
 	public Game() {
 		console = new Scanner(System.in);
 	}
-	
+
 	public void setup() {
 		// Determine how many players there are
 		do {
@@ -42,9 +34,9 @@ public class Game {
 				System.out.println("You must have at least 2 players but no more than 10 players\n");
 			}
 		} while (playerCount == 0);
-		
+
 		// Create players array and give players names
-		players = new ArrayList(playerCount);
+		players = new Player[playerCount];
 		System.out.println("Give each player a name in the order in which you want them to go.");
 		for (int i = 0; i < playerCount; i++) {
 			System.out.println("	Player " + (i + 1) + " of " + playerCount);
@@ -58,80 +50,34 @@ public class Game {
 					System.out.println("A valid name must be between 1 and 12 characters long");
 				}
 			} while (name.isEmpty());
-			players.set(i, new Player(name));
+			players[i] = new Player(name);
 		}
-		
-		// Display current players
 		System.out.print("The current players are: ");
 		for (int i = 0; i < playerCount; i++) {
-			System.out.print(players.get(i).getName());
+			System.out.print(players[i].getName());
 			if (i == playerCount - 2) {
 				System.out.print(" and ");
 			} else if (i != playerCount - 1) {
 				System.out.print(", ");
 			}
 		}
-		
-		// Create/fill pickup pile
-		discardPile = new Deck();
-		pickupPile = new Deck();
-		
-		for (Symbol symbol : Symbol.values()) {
-			for (Color color : Color.values()) {
-				// there are only four of each wild type and zeros
-				boolean isZero = symbol == Symbol.ZERO && color != Color.WILD;
-				boolean isWild = symbol == Symbol.WILD && color == Color.WILD;
-				if (isZero || isWild) {
-					pickupPile.addToTop(new Card(color, symbol));
-				} else if (symbol != Symbol.ZERO && symbol != Symbol.WILD && color != color.WILD) {
-					// Two of every other type of card
-					pickupPile.addToTop(new Card(color, symbol));
-					pickupPile.addToTop(new Card(color, symbol));
-				}
-			}
-		}
-		
-		// Shuffle pickup pile
-		pickupPile.shuffle();
-		
+
+		// Create/shuffle pickup pile
+		discardPile = new Pile();
+		pickupPile = new Pile();
+
 		// Dish out cards
-		for (int i = 0; i < 7; i++) {
-			// Each player get 7 cards
-			for (Player player : players) {
-				player.addToHand(pickupPile.drawFromTop());
-			}
-		}
-		
-		// Add card to discard pile to start game
-		discardPile.addToTop(pickupPile.drawFromTop());
+
 	}
-	
+
 	private void start() {
 		// While the game isn't finished, keep going around the table
 		boolean finished = false;
-		// Start with a random player each time
-		currentPlayer = players.get(new Random().nextInt(playerCount - 1));
 		while (!finished) {
-		
-		}
-	}
-	
-	private Player getNextPlayer() {
-		Player next = null;
-		int currentIndex = players.indexOf(currentPlayer);
-		if (!reversed) { // Its normal direction
-			if (currentIndex != playerCount - 1) {
-				next = players.get(currentIndex + 1);
-			} else {
-				next = players.get(0);
-			}
-		} else { // Game is reversed
-			if (currentIndex == 0) {
-				next = players.get(playerCount - 1);
-			} else {
-				next = players.get(currentIndex - 1);
+			for (int i = 0; i < playerCount; i++) {
+
 			}
 		}
-		return next;
 	}
+
 }
--- src/uno/Player.java
+++ src/uno/Player.java
@@ -1,21 +1,15 @@
 package uno;
 
-import adt.Card;
-
 public class Player {
-	
+
 	private String name;
 	private Hand playerHand;
-	
+
 	public Player(String name) {
 		this.name = name;
 	}
-	
+
 	public String getName() {
 		return name;
-	}
-	
-	public void addToHand(Card card) {
-		playerHand.addToHand(card);
 	}
 }
