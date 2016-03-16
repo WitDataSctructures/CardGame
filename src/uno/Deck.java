1521d3f3f691e97bab1db16fb9661336d600ae6a
Jake Mathews <mathewsj2@wit.edu>
2016-03-16 17:57:52 -0400

    Moved card generation to Deck class

--- src/uno/Deck.java
+++ src/uno/Deck.java
@@ -1,32 +1,52 @@
 package uno;
 
-import adt.Card;
-import adt.Pile;
-
 import java.util.Random;
+
+import adt.Card;
+import adt.Card.Color;
+import adt.Card.Symbol;
+import adt.Pile;
 
 public class Deck extends Pile {
 	
 	final static int NUM_OF_SHUFFLES = 2;
-
+	
+	public static Deck generateCards() {
+		Deck pile = new Deck();
+		for (Symbol symbol : Symbol.values()) {
+			for (Color color : Color.values()) {
+				// there are only four of each wild type and zeros
+				boolean isZero = symbol == Symbol.ZERO && color != Color.WILD;
+				boolean isWild = symbol == Symbol.WILD && color == Color.WILD;
+				if (isZero || isWild) {
+					pile.addToTop(new Card(color, symbol));
+				} else if (symbol != Symbol.ZERO && symbol != Symbol.WILD && color != Color.WILD) {
+					// Two of every other type of card
+					pile.addToTop(new Card(color, symbol));
+					pile.addToTop(new Card(color, symbol));
+				}
+			}
+		}
+		return pile;
+	}
+	
 	/**
-	 * removes cards from the top of the deck and places them in
-	 * a random spot in the deck.
+	 * removes cards from the top of the deck and places them in a random spot
+	 * in the deck.
 	 */
 	public void shuffle() {
 		Random random = new Random();
 		Card currentCard, placeAfter, placeBefore;
-		for (int i = this.getSize() * NUM_OF_SHUFFLES; i > 0; i--){
+		for (int i = this.getSize() * NUM_OF_SHUFFLES; i > 0; i--) {
 			currentCard = this.drawFromTop();
 			placeAfter = this.peekFromTop();
-			for (int j = random.nextInt(this.getSize()); j > 0; j--){
+			for (int j = random.nextInt(this.getSize()); j > 0; j--) {
 				placeAfter = placeAfter.getNext();
 			}
 			placeBefore = placeAfter.getNext();
 			placeAfter.setNext(currentCard);
 			currentCard.setNext(placeBefore);
 		}
-		
 		
 	}
 }
