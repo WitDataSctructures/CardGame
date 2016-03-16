d8edf7ae9c21aa21ef468f74d75a2b32e5740e6e
Jake Mathews <mathewsj2@wit.edu>
2016-03-16 09:05:17 -0400

    Added new framework for client server communications

--- src/uno/Game.java
+++ src/uno/Game.java
@@ -11,43 +11,41 @@ import adt.Card.Symbol;
 import uno.server.Server;
 
 public class Game {
-	
+
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		Game game = new Game();
 	}
-	
+
 	// Input manager
 	private Scanner console;
-	
+
 	// Game setup
 	private final static int MIN_PLAYERS = 2, MAX_PLAYERS = 10;
 	private int playerCount = 0;
 	private ArrayList<Player> players;
-	
+
 	// Game time variables
 	private Deck discardPile;
 	private Deck pickupPile;
 	Player currentPlayer;
 	boolean reversed = false;
-	
+
 	// Networking
 	private Server server;
 	private Socket socket;
-	
+
 	public Game() {
 		console = new Scanner(System.in);
 		if (connectToServer()) {
-		
+
 		}
 	}
-	
+
 	/**
 	 * Attempt to connect to an existing server or create one.
 	 * 
-	 * @return <b>True</b> if a server was successfully created or connected to.
-	 *         <b>False</b> if the player does not want to host and cannot
-	 *         connect to server.
+	 * @return <b>True</b> if a server was successfully created or connected to. <b>False</b> if the player does not want to host and cannot connect to server.
 	 */
 	@SuppressWarnings("unused")
 	private boolean connectToServer() {
@@ -57,7 +55,9 @@ public class Game {
 			if (server != null) {
 				System.out.println("You are the game host!");
 				System.out.println("Give the following address to anyone who wants to join your game.");
-				System.out.println("    Address = " + server.getServerAddress());
+				System.out.println("    Address = " + server.getServerAddress() + ":" + server.getServerPort());
+				socket = new Socket(server.getServerAddress(), server.getServerPort());
+				server.listen();
 			} else {
 				System.out.println("You are the not the game host. Enter an adress from a host to join their game.");
 				// TODO: Replace console reads with input manager
@@ -68,12 +68,15 @@ public class Game {
 					if (input.contains(":") && input.contains(".")) {
 						String address = input.split(":")[0];
 						String port = input.split(":")[1];
+						System.out.println("Connecting... Please wait...");
 						try {
 							socket = new Socket(address, Integer.parseInt(port));
+							// socket.connect(socket.getLocalSocketAddress());
 						} catch (Exception e) {
 							System.out.println("Could not connect to host @ " + input);
-							e.printStackTrace();
+							// e.printStackTrace();
 							success = false;
+							input = null;
 						}
 					} else {
 						input = null;
@@ -87,12 +90,11 @@ public class Game {
 		}
 		return success;
 	}
-	
+
 	/**
 	 * Gets the server instance.
 	 * 
-	 * @return <b>Server</b> instance. <b>Null</b> if player does not want to be
-	 *         host.
+	 * @return <b>Server</b> instance. <b>Null</b> if player does not want to be host.
 	 * @throws IOException
 	 */
 	private Server getServer() throws IOException {
@@ -113,14 +115,14 @@ public class Game {
 		}
 		return server;
 	}
-	
+
 	public void setupGame() {
 		// Display current players
-		
+
 		// Create/fill pickup pile
 		discardPile = new Deck();
 		pickupPile = new Deck();
-		
+
 		for (Symbol symbol : Symbol.values()) {
 			for (Color color : Color.values()) {
 				// there are only four of each wild type and zeros
@@ -135,10 +137,10 @@ public class Game {
 				}
 			}
 		}
-		
+
 		// Shuffle pickup pile
 		pickupPile.shuffle();
-		
+
 		// Dish out cards
 		for (int i = 0; i < 7; i++) {
 			// Each player get 7 cards
@@ -146,19 +148,19 @@ public class Game {
 				player.addToHand(pickupPile.drawFromTop());
 			}
 		}
-		
+
 		// Add card to discard pile to start game
 		discardPile.addToTop(pickupPile.drawFromTop());
 	}
-	
+
 	private void start() {
 		// While the game isn't finished, keep going around the table
 		boolean finished = false;
 		while (!finished) {
-		
+
 		}
 	}
-	
+
 	private void setupPlayers() {
 		// Determine how many players there are
 		do {
@@ -169,7 +171,7 @@ public class Game {
 				System.out.println("You must have at least 2 players but no more than 10 players\n");
 			}
 		} while (playerCount == 0);
-		
+
 		// Create players array and give players names
 		players = new ArrayList<Player>(playerCount);
 		System.out.println("Give each player a name in the order in which you want them to go.");
@@ -188,7 +190,7 @@ public class Game {
 			players.set(i, new Player(name));
 		}
 	}
-	
+
 	private Player getNextPlayer() {
 		Player next = null;
 		int currentIndex = players.indexOf(currentPlayer);
@@ -207,5 +209,5 @@ public class Game {
 		}
 		return next;
 	}
-	
+
 }
--- src/uno/server/Server.java
+++ src/uno/server/Server.java
@@ -1,27 +1,60 @@
 package uno.server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Server {
-	
+
+	public static void main(String[] args) {
+		try {
+			Server server = new Server();
+			while (true) {
+				server.listen();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	private final int PORT = 9090;
-	
+
 	private String name;
 	private ServerSocket serverSocket;
-	
+
 	public enum USER {
 		SERVER, USER
 	}
-	
+
 	public Server() throws IOException {
 		name = InetAddress.getLocalHost().getHostName();
-		serverSocket = new ServerSocket(PORT);
+		serverSocket = new ServerSocket(PORT, 50, InetAddress.getLocalHost());
 	}
-	
+
 	public String getServerAddress() throws UnknownHostException {
-		return serverSocket.getInetAddress().getHostAddress();
+		return InetAddress.getLocalHost().getHostAddress();
+	}
+
+	public int getServerPort() throws UnknownHostException {
+		return serverSocket.getLocalPort();
+	}
+
+	public void listen() {
+		try {
+			System.out.println("Listening on: " + getServerAddress() + ":" + getServerPort());
+			Socket client = serverSocket.accept();
+			DataInputStream in = new DataInputStream(client.getInputStream());
+			System.out.println(in.readUTF());
+			DataOutputStream out = new DataOutputStream(client.getOutputStream());
+			out.writeUTF("Thank you for connecting to " + serverSocket.getLocalSocketAddress() + "\nGoodbye!");
+			serverSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
