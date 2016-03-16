0c3558c8054ba6816a0d47e96c8b2e826d927cd0
Ford Polia <poliaf@wit.edu>
2016-03-16 09:33:02 -0400

    wrote getCard method in ConsoleInput

--- src/uno/ConsoleInput.java
+++ src/uno/ConsoleInput.java
@@ -3,6 +3,8 @@ package uno;
 import java.util.Scanner;
 
 import adt.Card;
+import adt.Card.Color;
+import adt.Card.Symbol;
 
 public class ConsoleInput extends InputManager {
 	
@@ -18,8 +20,107 @@ public class ConsoleInput extends InputManager {
 	 */
 	@Override
 	public Card getCard() {
-		// TODO Auto-generated method stub
-		return null;
+		String input = null, color, symbol;
+		Color cardColor = null;
+		Symbol cardSymbol = null;
+		while (input == null) {
+			input = console.nextLine().trim().toLowerCase();
+			color = input.split(" ")[0];
+			symbol = input.split(" ")[1];
+			switch(color){
+			case "red":
+				cardColor = Color.RED;
+				break;
+			case "yellow":
+				cardColor = Color.YELLOW;
+				break;
+			case "green":
+				cardColor = Color.GREEN;
+				break;
+			case "blue":
+				cardColor = Color.BLUE;
+				break;
+			default:
+				cardColor = Color.WILD;
+				break;
+			}
+			switch(symbol){
+			case "0":
+				cardSymbol = Symbol.ZERO;
+				break;
+			case "zero":
+				cardSymbol = Symbol.ZERO;
+				break;
+			case "1":
+				cardSymbol = Symbol.ONE;
+				break;
+			case "one":
+				cardSymbol = Symbol.ONE;
+				break;
+			case "2":
+				cardSymbol = Symbol.TWO;
+				break;
+			case "two":
+				cardSymbol = Symbol.TWO;
+				break;
+			case "3":
+				cardSymbol = Symbol.THREE;
+				break;
+			case "three":
+				cardSymbol = Symbol.THREE;
+				break;
+			case "4":
+				cardSymbol = Symbol.FOUR;
+				break;
+			case "four":
+				cardSymbol = Symbol.FOUR;
+				break;
+			case "5":
+				cardSymbol = Symbol.FIVE;
+				break;
+			case "five":
+				cardSymbol = Symbol.FIVE;
+				break;
+			case "6":
+				cardSymbol = Symbol.SIX;
+				break;
+			case "six":
+				cardSymbol = Symbol.SIX;
+				break;
+			case "7":
+				cardSymbol = Symbol.SEVEN;
+				break;
+			case "seven":
+				cardSymbol = Symbol.SEVEN;
+				break;
+			case "8":
+				cardSymbol = Symbol.EIGHT;
+				break;
+			case "eight":
+				cardSymbol = Symbol.EIGHT;
+				break;
+			case "9":
+				cardSymbol = Symbol.NINE;
+				break;
+			case "skip":
+				cardSymbol = Symbol.SKIP;
+				break;
+			case "reverse":
+				cardSymbol = Symbol.REVERSE;
+				break;
+			default:
+				if (input.contains("draw")){
+					if (cardColor == Color.WILD){
+						cardSymbol = Symbol.WILD_DRAW_FOUR;
+					}else{
+						cardSymbol = Symbol.DRAW_TWO;
+					}
+				} else if (cardColor == Color.WILD){
+					cardSymbol = Symbol.WILD;
+				}
+			}
+		}
+		return new Card(cardColor, cardSymbol);
 	}
 	
 	/**
