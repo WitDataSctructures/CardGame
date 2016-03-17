0b5e729c5f75ebfe1b23a049f8f4ed5266b2ca55
Jake Mathews <mathewsj2@wit.edu>
2016-03-16 23:28:26 -0400

    Fixed two bugs

--- src/uno/server/Server.java
+++ src/uno/server/Server.java
@@ -11,7 +11,7 @@ import uno.ConsoleInput;
 import uno.Deck;
 
 public class Server {
-	
+
 	public static void main(String[] args) {
 		try {
 			Server server;
@@ -25,32 +25,32 @@ public class Server {
 			e.printStackTrace();
 		}
 	}
-	
+
 	ConsoleInput console;
-	
+
 	private final static int DEFAULT_PORT = 9090;
 	private final static int MAX_CONNECTIONS = 10;
 	private final static int MIN_CONNECTIONS = 2;
 	public static final String REGEX = "|";
-	
+
 	private String name;
 	private ServerSocket connectionSocket;
-	
+
 	Deck pickup;
 	Deck discard;
-	
+
 	boolean started = false;
-	
+
 	private HashMap<String, ClientThread> clients;
-	
+
 	public enum USER {
 		SERVER, USER
 	}
-	
+
 	public Server() throws IOException {
 		this(DEFAULT_PORT);
 	}
-	
+
 	public Server(int port) throws IOException {
 		name = InetAddress.getLocalHost().getHostName();
 		connectionSocket = new ServerSocket(port, 50, InetAddress.getLocalHost());
@@ -58,15 +58,15 @@ public class Server {
 		clients = new HashMap<String, ClientThread>();
 		listen();
 	}
-	
+
 	public String getServerAddress() throws UnknownHostException {
 		return InetAddress.getLocalHost().getHostAddress();
 	}
-	
+
 	public int getServerPort() throws UnknownHostException {
 		return connectionSocket.getLocalPort();
 	}
-	
+
 	public void listen() {
 		while (true) {
 			try {
@@ -86,29 +86,30 @@ public class Server {
 			} catch (IOException e) {
 				System.out.println("Connection Closed");
 				e.printStackTrace();
-				
+
 			}
 		}
 		startGame();
 	}
-	
+
 	/**
 	 * Starts a game of UNO. Should only be called if the <i>listen()</i> method has already been called
 	 */
 	private void startGame() {
+		started = true;
 		pickup = Deck.generateCards();
 		pickup.shuffle(10);
 		discard = new Deck();
 		for (int i = 0; i < 7; i++) {
 			for (ClientThread client : clients.values()) {
-			
+
 			}
 		}
 		while (true) {
-		
+
 		}
 	}
-	
+
 	/**
 	 * Adds a client to the server's list of clients. Can only be done during the setup portion of the game.
 	 * 
@@ -119,9 +120,10 @@ public class Server {
 	 * @return <b>True</b> if successfully added to client list. <b>False</b> if user with that UUID is already connected
 	 */
 	public boolean addClient(String UUID, ClientThread client) {
+		System.out.println("Client UUID [" + UUID + "]");
 		return !started && (clients.putIfAbsent(UUID, client) == null);
 	}
-	
+
 	/**
 	 * Returns the server's socket
 	 * 
