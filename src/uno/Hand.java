2bc6c0fca96edf4d59991aab17c4811e2748e2a7
Jake Mathews <mathewsj2@wit.edu>
2016-03-17 16:33:40 -0400

    Added dishing out TODO fix hand bug

--- src/uno/Hand.java
+++ src/uno/Hand.java
@@ -1,54 +1,53 @@
 package uno;
+
 import java.util.ArrayList;
 
 import adt.Card;
 import adt.Pile;
 
-public class Hand extends Pile{
-	
-	
-	//Default Constructor
-	public Hand(){
+public class Hand extends Pile {
+
+	// Default Constructor
+	public Hand() {
 	}
-	
-	
-	//Returns an integer of how many cards currently in the player's hand.
-	public int getHandSize(){
+
+	// Returns an integer of how many cards currently in the player's hand.
+	public int getHandSize() {
 		return this.getSize();
 	}
-	
-	//Returns the playerHand.
-	public ArrayList<Card> getHand(){
+
+	// Returns the playerHand.
+	public ArrayList<Card> getHand() {
 		Card temp;
 		temp = this.peekFromTop();
 		ArrayList<Card> cardsInHand = new ArrayList<Card>();
-		for (int i = 0; i < this.getSize(); i++){
+		for (int i = 0; i < this.getSize(); i++) {
 			cardsInHand.add(temp);
 			temp = temp.getNext();
 		}
 		return cardsInHand;
 	}
-	
-	//Checks if a Card parameter exists in the player's hand.
-	public boolean isCardInHand(Card cardToLookFor){
+
+	// Checks if a Card parameter exists in the player's hand.
+	public boolean isCardInHand(Card cardToLookFor) {
 		ArrayList<Card> playerHand = getHand();
-		for(int i = 0; i < playerHand.size(); i++){
-			if(cardToLookFor == playerHand.get(i)){
+		for (int i = 0; i < playerHand.size(); i++) {
+			if (cardToLookFor == playerHand.get(i)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
 
-	//Add a Card parameter to the hand.
-	public void addToHand(Card cardToBeAdded){
+	// Add a Card parameter to the hand.
+	public void addToHand(Card cardToBeAdded) {
+		System.out.println("Adding " + cardToBeAdded);
 		this.addToTop(cardToBeAdded);
 	}
-	
-	//If the Card exists in the hand, remove it, otherwise do nothing.
-	public void removeFromHand(Card cardToBeRemoved){
-		if(isCardInHand(cardToBeRemoved)){
+
+	// If the Card exists in the hand, remove it, otherwise do nothing.
+	public void removeFromHand(Card cardToBeRemoved) {
+		if (isCardInHand(cardToBeRemoved)) {
 			this.remove(cardToBeRemoved);
 		}
 	}
--- src/uno/client/Client.java
+++ src/uno/client/Client.java
@@ -20,12 +20,12 @@ public class Client {
 			System.out.println("Invalid usage. Proper usage: uno [player_name]");
 		}
 	}
-	
+
 	Socket server;
-	ConsoleInput in;
+	ConsoleInput in; // TODO: Change to InputManager
 	Player player;
 	// String playerName;
-	
+
 	public Client(String name) {
 		// Get console input
 		in = new ConsoleInput();
@@ -51,39 +51,46 @@ public class Client {
 				run();
 			} catch (IOException e) {
 				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
 			}
 		}
 	}
-	
-	private void run() throws IOException {
+
+	private void run() throws IOException, ClassNotFoundException {
 		ObjectInputStream in = new ObjectInputStream(server.getInputStream());
 		ObjectOutputStream out = new ObjectOutputStream(server.getOutputStream());
-		
+
 		System.out.println("Attempting to connect to " + server.getRemoteSocketAddress());
-		out.writeUTF("N" + Server.REGEX + player.getName());
-		String result = in.readUTF();
-		if (result.equals("success")) {
+		out.writeObject(new ClientPacket("connect" + Server.REGEX + player.getName(), null, null, null));
+		ClientPacket result = (ClientPacket) in.readObject();
+		if (result.getMessage().equals("success")) {
 			System.out.println("Connected to server @ " + server.getRemoteSocketAddress());
 		} else {
 			System.out.println(result);
 			server = null;
 		}
-		
+
 		while (true) {
-			if (in.available() > 0) {
+			// System.out.println(in);
+			if (in.available() > 0 || true) {
 				try {
 					ClientPacket packet = (ClientPacket) in.readObject();
 					String message = packet.getMessage();
+					System.out.println("Message = [" + message + "]");
 					switch (message) {
 						case "dish_card":
 							// Server is trying to dish us out one card
 							Deck pickupPile = packet.getPickupPile();
+							// System.out.println("Drawn " + packet.getPickupPile().peekFromTop());
 							player.addToHand(pickupPile.drawFromTop());
 							packet.setMessage("success");
 							packet.setPickupPile(pickupPile);
 							out.writeObject(packet); // Return the deck with one less card and new message
 							break;
 						default:
+							packet.setMessage("no_command");
+							out.writeObject(packet);
 							System.out.println("Unkown packet message");
 							break;
 					}
